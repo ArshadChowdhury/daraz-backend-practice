4fe1ecdab4bafce93e5951cb5a81ae275b0235f3
Arshad Chowdhury <arshadchowdhury46@gmail.com>
2024-11-20 23:52:25 +0600

    Renamed tweet to Products module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,14 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './users/users.module';
-import { TweetModule } from './tweet/tweet.module';
+import { ProductsModule } from './products/tweet.module';
 
 ConfigModule.forRoot({
   envFilePath: '.env.local',
 });
 
 @Module({
-  imports: [ConfigModule.forRoot(), UserModule, TweetModule],
+  imports: [ConfigModule.forRoot(), UserModule, ProductsModule],
   controllers: [AppController],
   providers: [AppService],
 })
