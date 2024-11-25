c8af761072c452c56e1741df9a2d99d04219b2ec
Arshad Chowdhury <arshadchowdhury46@gmail.com>
2024-11-26 02:00:15 +0600

    Added typeorm and docker file to the project

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,13 +5,29 @@ import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './users/users.module';
 import { ProductsModule } from './products/tweet.module';
 import { CoffeesModule } from './coffees/coffees.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 ConfigModule.forRoot({
   envFilePath: '.env.local',
 });
 
 @Module({
-  imports: [ConfigModule.forRoot(), UserModule, ProductsModule, CoffeesModule],
+  imports: [
+    ConfigModule.forRoot(),
+    UserModule,
+    ProductsModule,
+    CoffeesModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'arshad',
+      database: 'postgres',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
