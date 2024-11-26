453c43ea5018a89b4678c3f958cef6ae69c7c808
Arshad Chowdhury <arshadchowdhury46@gmail.com>
2024-11-27 02:03:20 +0600

    Added pagination queries to psql db and made it work properly

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -13,15 +13,16 @@ import {
 import { CoffeesService } from './coffees.service';
 import { CreateCoffeeDto } from './dto/create-coffee.dto';
 import { UpdateCoffeeDto } from './dto/update-coffee.dto';
+import { PaginationQueryDto } from 'src/common/dto/pagination-query.dto/pagination-query.dto';
 
 @Controller('coffees')
 export class CoffeesController {
   constructor(private readonly coffeesService: CoffeesService) {}
 
   @Get()
-  findAll(@Query() query) {
-    const { limit, offset } = query;
-    return this.coffeesService.findAllCoffees(limit, offset);
+  findAll(@Query() paginationQuery: PaginationQueryDto) {
+    // const { limit, offset } = query;
+    return this.coffeesService.findAllCoffees(paginationQuery);
   }
 
   @Get(':id')
--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -9,22 +9,33 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateCoffeeDto } from './dto/create-coffee.dto';
 import { UpdateCoffeeDto } from './dto/update-coffee.dto';
+import { Flavor } from './entities/flavor.entity';
+import { PaginationQueryDto } from 'src/common/dto/pagination-query.dto/pagination-query.dto';
 
 @Injectable()
 export class CoffeesService {
   constructor(
     @InjectRepository(Coffee)
     private readonly coffeeRepository: Repository<Coffee>,
+    @InjectRepository(Flavor)
+    private readonly flavorRepository: Repository<Flavor>,
   ) {}
 
-  findAllCoffees(limit, offset) {
-    console.log(limit, offset);
+  findAllCoffees(paginationQuery: PaginationQueryDto) {
+    const { limit, offset } = paginationQuery;
 
-    return this.coffeeRepository.find();
+    return this.coffeeRepository.find({
+      relations: ['flavors'],
+      skip: offset,
+      take: limit,
+    });
   }
 
   async findOneCoffee(id) {
-    const coffee = await this.coffeeRepository.findOne({ where: { id } });
+    const coffee = await this.coffeeRepository.findOne({
+      where: { id },
+      relations: ['flavors'],
+    });
 
     if (!coffee) {
       throw new HttpException(
@@ -35,15 +46,29 @@ export class CoffeesService {
     return coffee;
   }
 
-  createCoffee(createCoffeeDto: CreateCoffeeDto) {
-    const coffee = this.coffeeRepository.create(createCoffeeDto);
+  async createCoffee(createCoffeeDto: CreateCoffeeDto) {
+    const flavors = await Promise.all(
+      createCoffeeDto.flavors.map((name) => this.preloadFlavorByName(name)),
+    );
+
+    const coffee = this.coffeeRepository.create({
+      ...createCoffeeDto,
+      flavors,
+    });
     return this.coffeeRepository.save(coffee);
   }
 
   async updateCoffee(id, updateCoffeeDto: UpdateCoffeeDto) {
+    const flavors =
+      updateCoffeeDto.flavors &&
+      (await Promise.all(
+        updateCoffeeDto.flavors.map((name) => this.preloadFlavorByName(name)),
+      ));
+
     const coffee = await this.coffeeRepository.preload({
       id: +id,
       ...updateCoffeeDto,
+      flavors,
     });
 
     if (!coffee) {
@@ -56,5 +81,17 @@ export class CoffeesService {
   async deleteCoffee(id) {
     const coffee = await this.coffeeRepository.findOne({ where: { id } });
     return this.coffeeRepository.remove(coffee);
+  }
+
+  private async preloadFlavorByName(name: string): Promise<Flavor> {
+    const existingFlavor = await this.flavorRepository.findOne({
+      where: { name },
+    });
+
+    if (existingFlavor) {
+      return existingFlavor;
+    }
+
+    return this.flavorRepository.create({ name });
   }
 }
